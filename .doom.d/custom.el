44a072cf4501695647c66ebf2dc58bea9087656c
NeddX <nedbeargt@gmail.com>
2024-11-26 23:14:29 +0400

    Minor

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -37,7 +37,6 @@
 ;; '(font-lock-variable-name-face ((t (:foreground "#bc9334"))))
 ;; '(show-paren-match-face ((t (:foreground "#78c241"))))
 ;; '(ts-fold-replacement-face ((t (:foreground unspecified :box nil :inherit font-lock-comment-face :weight light)))))
-;;(put 'narrow-to-region 'disabled nil)
 ;;(put 'customize-face 'disabled nil)
 ;;(put 'projectile-grep 'disabled nil)
 (custom-set-faces
@@ -46,3 +45,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'narrow-to-region 'disabled nil)
